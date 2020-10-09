df4dd49a83aed2389ffc8e9a64afc130dc25bb1f
isip_d.a.pahomov@mpt.ru <isip_d.a.pahomov@mpt.ru>
2020-10-09 15:14:59 +0300

    Добавьте файлы проекта.

---
+++ Web_Shop/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult Bedroom()
+        {
+            return View();
+        }
+        public ActionResult Children_Bedroom()
+        {
+            return View();
+        }
+        public ActionResult Kitchen()
+        {
+            return View();
+        }
+        public ActionResult Living_Room()
+        {
+            return View();
+        }
+
+        public ActionResult Product_card()
+        {
+            return View();
+        }
+
+        public ActionResult Personal_Account()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Web_Shop/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Web_Shop.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server" BackColor="#CC6699" BorderStyle="None" BorderWidth="0px" CellPadding="0" CellSpacing="0">
+        </asp:CheckBoxList>
+    </form>
+</body>
+</html>
